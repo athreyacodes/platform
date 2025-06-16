7d8453b8d4dbdd57fdca744b187c6a4d7ee68a0e
Athreya M R <athreyamr10@gmail.com>
2025-06-16 07:32:46 +0530

    html modified

--- apps/shell/src/app/nx-welcome.ts
+++ apps/shell/src/app/nx-welcome.ts
@@ -1,10 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nx-welcome',
-  imports: [CommonModule],
-  template: `SHELL APP 2`,
+  imports: [CommonModule, RouterModule],
+  template: `<a [routerLink]="'/portfolio'">Portfolio</a>`,
   styles: [],
   encapsulation: ViewEncapsulation.None,
 })
